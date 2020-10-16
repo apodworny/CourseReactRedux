31904fb53328216c9f4ae459077814f793bfa1c9
Alex Podworny <alex.podworny@hotmail.com>
2020-10-16 17:03:41 -0500

    Finished section 11

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -2,13 +2,9 @@ import React from 'react';
 import VideoItem from './VideoItem';
 
 class VideoList extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         const renderedList = this.props.videos.map(video => {
-            return <VideoItem video={video} />;
+            return <VideoItem key={video.id.videoId} onVideoSelect={this.props.onVideoSelect} video={video} />;
         });
         return <div className="ui relaxed divided list">{renderedList}</div>
     }
