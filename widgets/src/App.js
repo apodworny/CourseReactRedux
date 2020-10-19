a42a1a4a43182978274ce26ccc35ca8e5eb95bc1
Alex Podworny <alex.podworny@hotmail.com>
2020-10-19 16:58:45 -0500

    Section 13 - added custom routing

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -3,6 +3,8 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
 const items = [
     {
@@ -35,12 +37,22 @@ const options = [
 ];
 
 export default () => {
+    const [selected, setSelected] = useState(options[0]);
     return (
         <div>
-            {/* <Accordion items={items} /> */}
-            {/* <Search /> */}
-            {/* { showDropdown ? <Dropdown options={options} onSelectedChange={setSelected} selected={selected} /> : null } */}
-            <Translate />
+            <Header />
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown label="Select a color" options={options} selected={selected} onSelectedChange={setSelected} />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
         </div>
     );
 }
