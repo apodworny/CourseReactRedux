b8b3f38a7d8023592a100e20ad91843943aa7284
Alex Podworny <alex.podworny@hotmail.com>
2020-10-26 23:59:18 -0500

    Started Section 21 Handling Authentication with React

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,32 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-const PageOne = () => {
-    return (
-        <div>
-        <h1>Page One</h1>
-            <Link to="/pagetwo">Navigate to page two</Link>
-        </div>
-    )
-}
-
-const PageTwo = () => {
-    return (
-        <div>
-            <div>PageTwo</div>
-            <button>Click me</button>
-        </div>
-    );
-}
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 const App = () => {
     return (
-        <div>
+        <div className="ui container">
             <BrowserRouter>
-            <div>
-                <Route path="/" exact component={PageOne} />
-                <Route path="/pagetwo" component={PageTwo} />
-            </div>
+                <div>
+                    <Header />
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" exact component={StreamCreate} />
+                    <Route path="/streams/edit" exact component={StreamEdit} />
+                    <Route path="/streams/delete" exact component={StreamDelete} />
+                    <Route path="/streams/show" exact component={StreamShow} />
+                </div>
             </BrowserRouter>
         </div>
     );
